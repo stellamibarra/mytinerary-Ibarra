4c4ed52f356692c4c7b8a94094c28570e0202072
stellamibarra <stellamaristandil@gmail.com>
2023-07-31 21:16:54 -0300

    first commit

---
+++ mytinerary-Ibarra/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
